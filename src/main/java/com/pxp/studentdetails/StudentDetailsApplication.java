1b6ab41efc4c497d8153300fe3ff7579d9b1bf62
apaspxp <63920754+apaspxp@users.noreply.github.com>
2021-06-12 18:01:30 +0530

    Update StudentDetailsApplication.java

--- src/main/java/com/pxp/studentdetails/StudentDetailsApplication.java
+++ src/main/java/com/pxp/studentdetails/StudentDetailsApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class StudentDetailsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("");
 		SpringApplication.run(StudentDetailsApplication.class, args);
 	}
 
